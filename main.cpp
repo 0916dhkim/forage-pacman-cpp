8e3a49e6f1adb4ad32022ae01c742a54b90ec3a5
Donghyeon Kim <0916dhkim@gmail.com>
2022-03-13 16:56:06 -0400

    Lint

--- main.cpp
+++ main.cpp
@@ -1,25 +1,23 @@
 #include <gameclass.h>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
 
-    GameLoop    *loop;
+  GameLoop *loop;
 
-    if (argc == 1 || argc > 2)
-    {
-        std::cout << "Invalid number of parameters.\n";
-        std::cout << "Usage: ./pacman maps/map.txt\n";
-        exit(0);
-    }
+  if (argc == 1 || argc > 2) {
+    std::cout << "Invalid number of parameters.\n";
+    std::cout << "Usage: ./pacman maps/map.txt\n";
+    exit(0);
+  }
 
-    loop = new GameLoop(argv[1]);
+  loop = new GameLoop(argv[1]);
 
-    loop->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    loop->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    loop->setFixedSize(WIDTH, HEIGHT);
-    loop->setSceneRect(0, 0, WIDTH, HEIGHT);
-    loop->show();
-    loop->ft_roll_game();
-    return a.exec();
+  loop->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+  loop->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+  loop->setFixedSize(WIDTH, HEIGHT);
+  loop->setSceneRect(0, 0, WIDTH, HEIGHT);
+  loop->show();
+  loop->ft_roll_game();
+  return a.exec();
 }
--- pinky.h
+++ pinky.h
@@ -1,31 +1,30 @@
 #ifndef PINKY_H
 #define PINKY_H
 
-#include "ghost.h"
 #include "blinky.h"
 #include "clyde.h"
+#include "ghost.h"
 #include "inky.h"
 
-class       Blinky;
-class       Clyde;
-class       Inky;
+class Blinky;
+class Clyde;
+class Inky;
 
-class       Pinky : public Ghost
-{
+class Pinky : public Ghost {
 private:
-    Blinky  *blinky;
-    Clyde   *clyde;
-    Inky    *inky;
-    Q_OBJECT
+  Blinky *blinky;
+  Clyde *clyde;
+  Inky *inky;
+  Q_OBJECT
 public:
-    void    ft_set_default();
-    void    ft_calculate_point();
-    void    ft_find_pacman();
-    void    ft_set_friends(Blinky *bl, Clyde *cl, Inky *ink);
-    void    ft_find_path();
-            Pinky(QGraphicsScene *sc, int **map, PacMan *pc);
+  void ft_set_default();
+  void ft_calculate_point();
+  void ft_find_pacman();
+  void ft_set_friends(Blinky *bl, Clyde *cl, Inky *ink);
+  void ft_find_path();
+  Pinky(QGraphicsScene *sc, int **map, PacMan *pc);
 public slots:
-    void    ft_move_ghost();
+  void ft_move_ghost();
 };
 
 #endif // PINKY_H
